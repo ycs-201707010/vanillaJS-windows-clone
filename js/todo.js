2c0ca3fdf38cdd3485e1a50845c00cd7bae9529e
LSD <yell2735@naver.com>
2023-03-31 19:46:27 +0900

    03-31 commit
    ToDo 삭제 기능 지원 및 css 수정

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,35 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
 /** Todo list를 그리는 기능을 하는 함수. */
 function paintToDo(newToDo) {
   const li = document.createElement("li");
+
   const span = document.createElement("span");
+  span.innerText = newToDo;
+
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", (event) => {
+    const li = event.target.parentElement;
+    li.remove(); // HTML 태그를 지우는 함수
+  });
 
   li.appendChild(span);
+  li.appendChild(btn);
+
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
+  toDos.push(newToDo);
   toDoInput.value = "";
   paintToDo(newToDo);
 }
 
+console.log(toDos);
 toDoForm.addEventListener("submit", handleToDoSubmit);
