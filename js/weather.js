280da0db8d1a1150b17d2e561168779be2277b0d
LSD <yell2735@naver.com>
2023-04-01 15:49:58 +0900

    04-01 commit
    주요 기능 구현 완료

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,18 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   console.log(`you live in ${lat}, ${lon}`);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  console.log(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = `/ ${Math.floor(data.main.temp)}ºC ${
+        data.weather[0].main
+      }`;
+    });
 }
 
 function onGeoError() {
